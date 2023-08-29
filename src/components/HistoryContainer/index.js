4bcc197a495a0ff32f74129ffd0240eaf0cbfd24
Rohith Patel <rohithgithub999@gmail.com>
2023-08-29 07:29:20 +0000

    react Browser History

--- src/components/HistoryContainer/index.js
+++ src/components/HistoryContainer/index.js
@@ -4,20 +4,58 @@ import HistoryListItems from '../HistoryListItems/index'
 import './index.css'
 
 class HistoryContainer extends Component {
-  state = {searchInput: ''}
+  constructor(props) {
+    // The constructor method is a special method of a class for creating and initializing an object of that class.
+    super(props)
+    this.state = {
+      // properties
+      searchInput: '',
+      updatedArrayResults: props.initialHistoryList,
+    }
+  }
 
   searchingInput = event => {
+    // methods
     const val = event.target.value
     this.setState({searchInput: val})
   }
 
-  render() {
-    const {initialHistoryList} = this.props
-    const {searchInput} = this.state
-
-    const searchRelatedArray = initialHistoryList.filter(eachObjectFilter =>
-      eachObjectFilter.title.toLowerCase().includes(searchInput),
+  deleteHistoryItem = idNum => {
+    // methods
+    const {updatedArrayResults} = this.state
+    const afterDeleteArray = updatedArrayResults.filter(
+      eachDeleteArray => eachDeleteArray.id !== idNum,
     )
+
+    this.setState({updatedArrayResults: afterDeleteArray})
+  }
+
+  render() {
+    // methods
+    // const {initialHistoryList} = this.props
+    const {searchInput, updatedArrayResults} = this.state
+    console.log(updatedArrayResults)
+
+    const searchRelatedArray = updatedArrayResults.filter(eachObjectFilter =>
+      eachObjectFilter.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    const renderAuth = () => {
+      if (searchRelatedArray.length !== 0) {
+        return (
+          <ul className="ulContainer">
+            {searchRelatedArray.map(eachObject => (
+              <HistoryListItems
+                eachObjectProp={eachObject}
+                deleteHistoryItemProp={this.deleteHistoryItem}
+                key={eachObject.id}
+              />
+            ))}
+          </ul>
+        )
+      }
+      return <p>There is no history to show</p>
+    }
 
     return (
       <div className="bg">
@@ -47,17 +85,7 @@ class HistoryContainer extends Component {
           </div>
         </div>
 
-        <div className="lowerCardBg">
-          <ul className="ulContainer">
-            {searchRelatedArray.map(eachObject => (
-              <HistoryListItems
-                eachObjectProp={eachObject}
-                deleteHistoryItemProp={this.deleteHistoryItem}
-                key={eachObject.id}
-              />
-            ))}
-          </ul>
-        </div>
+        <div className="lowerCardBg">{renderAuth()}</div>
       </div>
     )
   }
